1a3f370dccecd3d85bf011e274eb908f79997cf6
Jon Johnson <jonjonsonjr@gmail.com>
2013-10-12 19:24:39 -0400

    Added history, should be good enough to play with now

--- public/app.js
+++ public/app.js
@@ -4,23 +4,25 @@ var width = 800;
 var height = 600;
 var r = 250;
 var displayURL = $('#url');
+var rootURL = displayURL.val();
 var visited = [];
+var cache = {};
+var history = [];
 
 function load(url) {
   visited.push(url);
+  rootURL = url;
   displayURL.val(url);
   $.get('/url?q=' + url, function (data) {
+    cache[url] = data;
     draw(data);
-  }).fail(function (err) {
-    console.log(err);
-  });
+  }).fail(console.log);
 }
 
 function draw(data) {
-  console.log(visited);
   d3.select('svg').remove(); 
   var svg = graph.append('svg').attr('width', width).attr('height', height);
-  var nodeRadius = 15 + 30 / data.length;
+  var nodeRadius = 15 + 30 / (1 + data.length);
   
   var links = data.map(function (d, i) {
     return {
@@ -36,14 +38,14 @@ function draw(data) {
       x: width / 2 + (r + offset) * Math.cos(theta),
       y: height / 2 + (r + offset) * Math.sin(theta),
       href: d,
-      fixed: true,
+      fixed: true
     };
   });
 
   nodes.unshift({
     x: width / 2,
     y: height / 2,
-    href: displayURL.val(),
+    href: rootURL,
     fixed: true
   });
 
@@ -61,7 +63,10 @@ function draw(data) {
     .attr("x1", function (d) { return d.source.x; })
     .attr("y1", function (d) { return d.source.y; })
     .attr("x2", function (d) { return d.target.x; })
-    .attr("y2", function (d) { return d.target.y; });
+    .attr("y2", function (d) { return d.target.y; })
+    .attr('stroke', function (d) {
+      return ($.inArray(d.target.href, visited) !== -1) ? '#55b' : '#aaa';
+    });
 
   var node = svg.selectAll('.node');
   node = node.data(nodes)
@@ -70,25 +75,15 @@ function draw(data) {
     .attr('class', 'node')
     .attr('r', nodeRadius)
     .attr('fill', function (d) {
-      console.log(d.href);
       return ($.inArray(d.href, visited) !== -1) ? '#55b' : '#aaa';
     })
-    .on('click', function (d) {
-      console.log(d.href);
-      if (d.index != 0) {
-        if (d.prefetch) {
-          draw(d.prefetch);
-        } else {
-          load(d.href);
-        }
-      }
-    })
+    .on('click', click)
     .attr('transform', function (d) {
       return 'translate(' + d.x + ', ' + d.y + ')';
     })
     .on('mouseover', function (d) {
       if (!d.prefetch) {
-        console.log(d.prefetch);
+        d.prefetch = true;
         prefetch(d);
       }
 
@@ -97,7 +92,42 @@ function draw(data) {
 }
 
 function prefetch(node) {
+  if (node.index === 0) return;
+
+  if (cache[node.href]) {
+    return node.prefetch = cache[node.href];
+  }
+
   $.get('http://localhost:3000/url?q=' + node.href, function (data) {
+    cache[node.href] = data;
     node.prefetch = data;
   });
+}
+
+function click(node) {
+  if (node.index === 0) {
+    if (history.length == 0) return;
+    var url = history.pop();
+    rootURL = url;
+    displayURL.val(url);
+    draw(cache[url]);
+    return;
+  }
+  
+  if (node.prefetch === undefined) {
+    return load(node.href);
+  }
+  
+  if (node.prefetch === true) {
+    console.log('waiting');
+    setTimeout(function () {
+      click(node)
+    }, 100);
+  } else {
+    history.push(rootURL);
+    rootURL = node.href;
+    displayURL.val(node.href);
+    visited.push(node.href);
+    draw(node.prefetch);
+  }
 }
--- server.js
+++ server.js
@@ -1,36 +1,22 @@
 var express = require('express');
-var requests = require('request');
 var phantom = require('node-phantom');
-var fs = require('fs');
-var util = require('util');
 var app = express();
 
 app.use(express.static(__dirname + '/public'));
 
 app.get('/url', function (req, res) {
   var url = req.query.q;
-  console.log(url);
-
+  
   phantom.create(function (err, ph) {
-    if (err) {
-      res.end();
-      ph.close();
-      return console.log(err);
-    }
+    if (err) return error(res, err, ph);
 
     return ph.createPage(function (err, page) {
-      if (err) {
-        res.end();
-        ph.exit();
-        return console.log(err);
-      }
+      if (err) return error(res, err, ph);
+      
       return page.open(url, function (err, status) {
         page.includeJs('http://localhost:3000/jquery-2.0.3.min.js', function (err) {
-          if (err) {
-            res.end();
-            ph.exit();
-            return console.log(err);
-          }
+          if (err) return error(res, err, ph);
+          
           return page.evaluate(function () {
             var hrefs = [];
 
@@ -42,14 +28,10 @@ app.get('/url', function (req, res) {
 
             return hrefs;
           }, function (err, data) {
-            if (err) {
-              res.end();
-              ph.exit();
-              return console.log(err);
-            }
+            if (err) return error(res, err, ph);
 
+            console.log(url + ' - ' + data.length);
             res.json(data);
-            console.log(data);
             ph.exit();
           });
         });
@@ -57,5 +39,11 @@ app.get('/url', function (req, res) {
     });
   });
 });
+
+function error(res, err, ph) {
+  res.end();
+  ph.exit();
+  return console.log(err);
+}
 
 app.listen(3000);
